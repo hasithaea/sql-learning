5f926c4544695c7632feca1afbbce9818d3f1f4e
hasithaea <hasitha2k4@gmail.com>
2026-09-02 04:46:42 +0530

    insert values into distribute table

--- assignment-03/queries/distribute-table-creation.sql
+++ assignment-03/queries/distribute-table-creation.sql
@@ -1,1 +1,9 @@
 create table distribute (itCode char(4), bsCode char(4), qty int, date date, primary key (itCode, bsCode));
+
+insert into distribute values ("IT10", "BS01", 30, "2025-04-08");
+insert into distribute values ("IT10", "BS03", 3, "2025-04-02");
+insert into distribute values ("IT10", "BS04", 10, "2025-04-25");
+insert into distribute values ("IT11", "BS01", 25, NULL);
+insert into distribute values ("IT11", "BS02", 12, "2025-05-07");
+insert into distribute values ("IT13", "BS04", 60, "2025-05-09");
+insert into distribute values ("IT16", "BS02", 5, NULL);
